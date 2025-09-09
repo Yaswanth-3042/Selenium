3a93f42349996b3b4724b53ff9576914a8ab06a9
Yaswanth Ikutti <103446119+Yaswanth-3042@users.noreply.github.com>
2025-09-09 19:44:00 +0530

    New Test Java Class Added and Selenium URL added

---
+++ src/fileupload/TestJavaClass.java
@@ -0,0 +1,9 @@
+package fileupload;
+
+public class TestJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+	}
+
+}
--- src/pagescrollDownandup/ScrollDownUp.java
+++ src/pagescrollDownandup/ScrollDownUp.java
@@ -1,14 +1,20 @@
 package pagescrollDownandup;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class ScrollDownUp {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void seleniumdev() {
+	  driver.get("https://www.selenium.dev");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
