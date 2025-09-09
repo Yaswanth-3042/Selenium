75aae1965f6a2aced5b3937c240228c1eab36fe2
Yaswanth Ikutti <103446119+Yaswanth-3042@users.noreply.github.com>
2025-09-09 19:28:10 +0530

    AfterTest Annotations added

--- src/tAbsandWindows/NewTabsSwitches.java
+++ src/tAbsandWindows/NewTabsSwitches.java
@@ -3,6 +3,7 @@ package tAbsandWindows;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class NewTabsSwitches {
@@ -15,6 +16,11 @@ public class NewTabsSwitches {
   public void beforeTest() {
 	  driver = new ChromeDriver();
 	  driver.manage().window().maximize();
+  }
+  
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
   }
 
 }
