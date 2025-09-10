bbe3c83a889edbe91348bb7bacaf19d706f56c3c
Yaswanth Ikutti <103446119+Yaswanth-3042@users.noreply.github.com>
2025-09-10 18:30:25 +0530

    Update ScrollDownUp class added aftertest in quit method by Lokesh.java

--- src/pagescrollDownandup/ScrollDownUp.java
+++ src/pagescrollDownandup/ScrollDownUp.java
@@ -4,17 +4,23 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 
 public class ScrollDownUp {
 	WebDriver driver;
   @Test
   public void seleniumdev() {
-	  driver.get("https://www.selenium.dev");
+	  driver.get("https://www.selenium.dev/");
   }
   @BeforeTest
   public void beforeTest() {
 	  driver = new ChromeDriver();
 	  driver.manage().window().maximize();
+  }
+
+@AfterTest
+  public void afterTest() {
+	  driver.quit();
   }
 
 }
