a4c72e960f3ecf02542a245c7bd657dcf8dfba30
Yaswanth Ikutti <103446119+Yaswanth-3042@users.noreply.github.com>
2025-09-10 18:57:00 +0530

    xyz

--- src/pagescrollDownandup/ScrollDownUp.java
+++ src/pagescrollDownandup/ScrollDownUp.java
@@ -2,7 +2,6 @@ package pagescrollDownandup;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
